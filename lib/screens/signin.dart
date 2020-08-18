7e8e4a8730bccfa4530386c4b7ebce4d5b2385c7
Kyou13 <kyou1013@gmail.com>
2020-08-19 01:26:55 +0900

    add signin screens

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -1,14 +1,100 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:lograph/common/constants.dart';
+import 'package:lograph/common/rounded_button.dart';
+import 'package:lograph/screens/category_list.dart';
 
 class Signin extends StatefulWidget {
   static const String id = 'signin';
+  final String title = 'ログイン';
   @override
   _SigninState createState() => _SigninState();
 }
 
 class _SigninState extends State<Signin> {
+  final _auth = FirebaseAuth.instance;
+  bool isShowSpinner = false;
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(24),
+        child: ModalProgressHUD(
+          inAsyncCall: isShowSpinner,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                      height: 200, child: Image.asset('images/logo.png')),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 42,
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'メールアドレスを入力してください'),
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Container(
+                height: 42,
+                child: TextField(
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration:
+                      kTextFieldDecoration.copyWith(hintText: 'パスワードを入力してください'),
+                ),
+              ),
+              RoundedButton(
+                title: 'ログイン',
+                color: Colors.blueAccent,
+                onPressed: () async {
+                  setState(() {
+                    isShowSpinner = true;
+                  });
+                  try {
+                    final user = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+                    if (user != null) {
+                      Navigator.pushNamed(context, CategoryList.id);
+                    }
+                    setState(() {
+                      isShowSpinner = false;
+                    });
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -6,6 +6,7 @@ import 'package:lograph/common/rounded_button.dart';
 import 'package:lograph/screens/category_list.dart';
 import 'package:lograph/screens/signin.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class Signup extends StatefulWidget {
   static const String id = 'signup';
@@ -15,7 +16,7 @@ class Signup extends StatefulWidget {
 
 class _SignupState extends State<Signup> {
   final _auth = FirebaseAuth.instance;
-  bool showSpinner = false;
+  bool isShowSpinner = false;
   String email;
   String password;
 
@@ -25,131 +26,135 @@ class _SignupState extends State<Signup> {
         backgroundColor: Colors.white,
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Hero(
-                    tag: 'logo',
-                    child: Container(
-                      height: 150,
-                      child: Image.asset('images/logo.png'),
+          child: ModalProgressHUD(
+            inAsyncCall: isShowSpinner,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Hero(
+                      tag: 'logo',
+                      child: Container(
+                        height: 150,
+                        child: Image.asset('images/logo.png'),
+                      ),
                     ),
+                    Text(
+                      'Lograph',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 42,
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'メールアドレスを入力してください'),
                   ),
-                  Text(
-                    'Lograph',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 42,
-                child: TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  decoration: kTextFieldDecoration.copyWith(
-                      hintText: 'メールアドレスを入力してください'),
                 ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Container(
-                height: 42,
-                child: TextField(
-                  obscureText: true,
-                  textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    password = value;
-                  },
-                  decoration:
-                      kTextFieldDecoration.copyWith(hintText: 'パスワードを入力してください'),
+                SizedBox(
+                  height: 8,
                 ),
-              ),
-              RoundedButton(
-                title: '新規登録',
-                color: Colors.blueAccent,
-                onPressed: () async {
-                  setState(() {
-                    showSpinner = true;
-                  });
-                  try {
-                    final newUser = await _auth.createUserWithEmailAndPassword(
-                        email: email, password: password);
-                    if (newUser != null) {
-                      Navigator.pushNamed(context, CategoryList.id);
-                    }
+                Container(
+                  height: 42,
+                  child: TextField(
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                        hintText: 'パスワードを入力してください'),
+                  ),
+                ),
+                RoundedButton(
+                  title: '新規登録',
+                  color: Colors.blueAccent,
+                  onPressed: () async {
                     setState(() {
-                      showSpinner = false;
+                      isShowSpinner = true;
                     });
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Divider(
-                      color: Colors.grey,
-                      height: 10,
+                    try {
+                      final newUser =
+                          await _auth.createUserWithEmailAndPassword(
+                              email: email, password: password);
+                      if (newUser != null) {
+                        Navigator.pushNamed(context, CategoryList.id);
+                      }
+                      setState(() {
+                        isShowSpinner = false;
+                      });
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey,
+                        height: 10,
+                      ),
                     ),
-                  ),
-                  Text(
-                    '別サービスでログイン',
-                    style: TextStyle(
-                      color: Colors.grey,
+                    Text(
+                      '別サービスでログイン',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: Divider(
-                      color: Colors.grey,
-                      height: 10,
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey,
+                        height: 10,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  CircleButton(
-                    icon: Icon(MdiIcons.twitter),
-                    color: Color.fromRGBO(29, 161, 242, 1),
-                    onPressed: () {},
-                  ),
-                  CircleButton(
-                    icon: Icon(MdiIcons.facebook),
-                    color: Color.fromRGBO(59, 89, 152, 1),
-                    onPressed: () {},
-                  ),
-                  CircleButton(
-                    icon: Icon(MdiIcons.google),
-                    color: Color.fromRGBO(219, 68, 55, 1),
-                    onPressed: () {},
-                  )
-                ],
-              ),
-              RoundedButton(
-                title: 'ログイン',
-                color: Colors.blueGrey[200],
-                onPressed: () {
-                  Navigator.pushNamed(context, Signin.id);
-                },
-              ),
-            ],
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CircleButton(
+                      icon: Icon(MdiIcons.twitter),
+                      color: Color.fromRGBO(29, 161, 242, 1),
+                      onPressed: () {},
+                    ),
+                    CircleButton(
+                      icon: Icon(MdiIcons.facebook),
+                      color: Color.fromRGBO(59, 89, 152, 1),
+                      onPressed: () {},
+                    ),
+                    CircleButton(
+                      icon: Icon(MdiIcons.google),
+                      color: Color.fromRGBO(219, 68, 55, 1),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+                RoundedButton(
+                  title: 'ログイン',
+                  color: Colors.blueGrey[200],
+                  onPressed: () {
+                    Navigator.pushNamed(context, Signin.id);
+                  },
+                ),
+              ],
+            ),
           ),
         ));
   }
