dd3c0473b022900ae42ff4b9d227c2eca3badc71
Kyou13 <kyou1013@gmail.com>
2020-08-19 10:13:42 +0900

    add bottomNavigationBar to CategoryList

--- lib/screens/category_list.dart
+++ lib/screens/category_list.dart
@@ -1,14 +1,80 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+final _firestore = Firestore.instance;
+FirebaseUser loggedInUser;
 
 class CategoryList extends StatefulWidget {
   static const String id = 'category_list';
+  final String title = 'ホーム';
   @override
   _CategoryListState createState() => _CategoryListState();
 }
 
 class _CategoryListState extends State<CategoryList> {
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void getLogs() async {
+    final logs = await _firestore.collection('logs').getDocuments();
+    for (final log in logs.documents) {}
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.blue,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            color: Colors.white,
+            onPressed: () => {},
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Text('AAA'),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.formatListBulleted),
+            title: Text('ログ一覧'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.chartLine),
+            title: Text('グラフ一覧'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.account),
+            title: Text('プロフィール'),
+          ),
+        ],
+      ),
+    );
   }
 }
